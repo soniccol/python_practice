2c02921c57cf1e92435d882a4928b8b3938b64e6
Tai-An Chen <soniccol@gmail.com>
2017-03-08 15:21:14 +0800

    Add google api verified python code in silent mode. Be sure to use this parameter --noauth_local_webserver

---
+++ get_auth_token.py
@@ -0,0 +1,36 @@
+'''
+    This script will attempt to open your webbrowser,
+    perform OAuth 2 authentication and print your access token.
+
+    It depends on two libraries: oauth2client and gflags.
+
+    To install dependencies from PyPI:
+
+    $ pip install python-gflags oauth2client
+
+    Then run this script:
+
+    $ python get_oauth2_token.py
+    
+    This is a combination of snippets from:
+    https://developers.google.com/api-client-library/python/guide/aaa_oauth
+'''
+
+from oauth2client.client import OAuth2WebServerFlow
+from oauth2client import tools
+from oauth2client.file import Storage
+
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+
+
+flow = OAuth2WebServerFlow(client_id=CLIENT_ID,
+                           client_secret=CLIENT_SECRET,
+                           scope='https://www.googleapis.com/auth/youtube',
+                           redirect_uri='http://example.com/auth_return')
+
+storage = Storage('creds.data')
+
+credentials = tools.run_flow(flow, storage)
+
+print "access_token: %s" % credentials.access_token
